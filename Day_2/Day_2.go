78383c65a36b473bc3f4538644ec4dc73b5538f8
Felix <flexearl@outlook.com>
2023-12-03 14:04:32 +0000

    Adding Day 2

--- Day_2/Day_2.go
+++ Day_2/Day_2.go
@@ -16,14 +16,14 @@ func Part1(lines []string) {
 		for j := 0; j < len(semiColonSplitted) && valid; j++ {
 			valid = checkRightAmount(semiColonSplitted[j])
 		}
-		fmt.Println(valid)
+
 		if valid {
 			spaceSplitted := strings.Split(colonSplitted[0], " ")
 			id, err := strconv.Atoi(spaceSplitted[1])
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println("id", id)
+
 			total += id
 		}
 	}
@@ -41,17 +41,15 @@ func checkRightAmount(line string) bool {
 	fmt.Println(commaSplitted)
 	for i := 0; i < len(commaSplitted) && valid; i++ {
 		spaceSplitted := strings.Split(commaSplitted[i], " ")
-		fmt.Println(spaceSplitted[0], spaceSplitted[1])
+
 		amount, err := strconv.Atoi(spaceSplitted[0])
-		fmt.Println(amount)
+
 		if err != nil {
 			log.Fatal(err)
 		}
 		key := spaceSplitted[1]
-		key = strings.ReplaceAll(key, " ", "")
-		fmt.Println(len(key))
-		fmt.Println(maxes[key])
-		valid = maxes[spaceSplitted[1]] >= amount
+		key = strings.TrimSpace(key)
+		valid = maxes[key] >= amount
 
 	}
 	return valid
